03d38a52e223072d59e9b3a61ef35a20bccf4894
hnonstoph <nghuyha2000@gmail.com>
2022-04-20 09:05:40 +0700

    Initial Commit

---
+++ ConceptCubeTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConceptCubeTest
+//
+//  Created by Huy Hà on 4/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
